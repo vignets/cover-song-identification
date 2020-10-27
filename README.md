d4acbe2104e67157a4f52ee5baf4e0edef2689b0
Vignesh Pillay <vignesh.umashankar@gmail.com>
2020-10-28 03:06:33 +0530

    Added more options in preprocess for audio dir

--- README.md
+++ README.md
@@ -5,21 +5,77 @@ Cover Song Identification with Siamese Network. Embeddings can be generated with
 Dataset taken from [Da-Tacos](https://github.com/MTG/da-tacos), which consists of subsets i.e benchmark and coveranalysis. Benchmark subset has 15000 songs with 1000 unique works each with 13 performances (2000 songs were added as noise), whereas Coveranalysis subset has 10000 songs with 5000 unique works each with 2 performances. You can find more in their official github [repo](https://github.com/MTG/da-tacos).
 
 ## Preprocess
-```
-➜ python preprocess.py --help                                                                                     (coversong) 
-Usage: preprocess.py [OPTIONS]
 
-  script to downsize the da-tacos dataset, generates a single h5 file for
+```
+➜ python preprocess.py --help
+Usage: preprocess.py [OPTIONS] COMMAND [ARGS]...
+
+  script contains two subcommands to either generates a single h5 file from
+  audio directory or da-tacos dataset
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  da-tacos         command to downsize the da-tacos dataset, generates a...
+  from-audio-dir  command to convert audio files directly to a single h5...
+
+```
+### Generating from da-tacos dataset
+```
+➜ python preprocess.py da-tacos --help
+Usage: preprocess.py da-tacos [OPTIONS]
+
+  command to downsize the da-tacos dataset, generates a single h5 file for
   each subset
 
 Options:
-  -d, --data-dir PATH             path to datasets folder  [default: datasets]
+  -d, --data-dir PATH             path to datasets folder which contains da-
+                                  tacos_benchmark_subset_single_files/ and da-
+                                  tacos_coveranalysis_subset_single_files/
+                                  folders  [default: datasets]
+
   -p, --pcp-features [crema|chroma_cens|hpcp]
                                   select pcp feature (use this option multiple
                                   times for more than one selection)
                                   [default: crema]
 
-  --spect-len INTEGER             Resized Spectral Length  [default: 500]
+  -s, --spect-len INTEGER         Resized output spectral length  [default:
+                                  500]
+
+  --help                          Show this message and exit.
+
+```
+### Generating from an audio directory
+```
+➜ python preprocess.py from-audio-dir --help
+Usage: preprocess.py from-audio-dir [OPTIONS]
+
+  command to convert audio files directly to a single h5 file for testing
+  and evaluation.
+
+Options:
+  -a, --audio-dir PATH            path to audio folder  [required]
+  -o, --output-dir PATH           path to output folder, where the script will
+                                  generate an intermediate features folder and
+                                  single h5 file from that folder.  [default:
+                                  datasets]
+
+  -p, --pcp-features [crema|chroma_cens|hpcp]
+                                  select pcp feature (use this option multiple
+                                  times for more than one selection)
+                                  [default: crema]
+
+  -r, --run-mode [parallel|single]
+                                  Whether to run the extractor in single or
+                                  parallel mode.  [default: parallel]
+
+  -n, --workers INTEGER           No of workers in parallel mode  [default:
+                                  -1]
+
+  -s, --spect-len INTEGER         Resized output spectral length  [default:
+                                  500]
+
   --help                          Show this message and exit.
 
 ```
--- preprocess.py
+++ preprocess.py
@@ -6,16 +6,47 @@ import cv2
 import deepdish as dd
 import numpy as np
 import pandas as pd
+from acoss.extractors import batch_feature_extractor
 from tqdm import tqdm
 
 
-def load(song, feature_type, spect_len):
-    feature_spec = dd.io.load(song, f"/{feature_type}")
+def dir_to_df(data_dir):
+    """Converts folder structure to pandas dataframe
+
+    Args:
+        data_dir ([Path]): Path to folder
+
+    Returns:
+        [pd.DataFrame]: dataframe
+    """
+    data_track = list()
+    for track in data_dir.glob("*/*"):
+        track_name = track.stem
+        work_name = track.parent.stem
+        data_track.append([work_name, track_name])
+
+    data_track_df = pd.DataFrame(data_track, columns=["work_id", "track_id"])
+    return data_track_df
+
+
+def downsize(feature_spec, spect_len):
+    """converts input array to a fixed length and downsizes it to desired spect_len.
+
+    Args:
+        feature_spec (np.ndarray): input array
+        spect_len (int): resized output spectral length
+
+    Returns:
+        [np.ndarray]: output array
+    """
+
     feature_spec = feature_spec.astype(np.float32)
     if feature_spec.shape[1] == 12:
         feature_spec = feature_spec.T
 
-    # convert to fixed length
+    # convert to fixed length of 180 sec
+    # hop leangth is 512 and 44100 sampling rate
+    # (44100 / 512) * 180 = approx. 15500 time frames
     desired_spect_len = 15500
     if feature_spec.shape[1] >= desired_spect_len:
         feature_spec = feature_spec[:, :desired_spect_len]
@@ -33,15 +64,66 @@ def load(song, feature_type, spect_len):
     return feature_spec
 
 
-@click.command(
-    help="script to downsize the da-tacos dataset, "
+def dir_to_h5(data_dir, output_file, pcp_features, spect_len):
+
+    """Packs all feature .h5 files inside folders and subfolders into a single h5 file.
+
+    Args:
+        data_dir (Path): path to feature dir containing h5 files
+        output_file (Path): full path name to output h5 file
+        pcp_features (tuple): sequence containing all pcp features to include
+        spect_len (int): resized output spectral length
+    """
+    store = pd.HDFStore(output_file, mode="w")
+
+    for feature_type in pcp_features:
+        data_track_df = dir_to_df(data_dir)
+        work_id_max_len = data_track_df.work_id.map(len).max()
+        track_id_max_len = data_track_df.track_id.map(len).max()
+        rec = data_track_df.to_records(index=False)
+        index = pd.MultiIndex.from_tuples(
+            [(*r, b) for r, b in itertools.product(rec, range(12))],
+            names=["work_id", "track_id", "chroma_bins"],
+        )
+
+        feature_specs = np.empty(
+            (data_track_df.shape[0], 12, spect_len), dtype=np.float32
+        )
+
+        for row in tqdm(data_track_df.itertuples(), total=data_track_df.shape[0]):
+            track_file = data_dir / row.work_id / row.track_id
+            feature_spec = dd.io.load(track_file.with_suffix(".h5"), f"/{feature_type}")
+            feature_specs[row.Index, :, :] = downsize(feature_spec, spect_len)
+
+        df = pd.DataFrame(
+            feature_specs.reshape(-1, feature_specs.shape[-1]), index=index
+        )
+
+        store.append(
+            feature_type,
+            df,
+            min_itemsize={"work_id": work_id_max_len, "track_id": track_id_max_len},
+        )
+
+    store.close()
+
+
+@click.group(
+    help="script contains two subcommands to either generates a single h5 file from audio directory or da-tacos dataset"
+)
+def preprocess():
+    pass
+
+
+@preprocess.command(
+    help="command to downsize the da-tacos dataset, "
     "generates a single h5 file for each subset"
 )
 @click.option(
     "--data-dir",
     "-d",
     type=click.Path(exists=True),
-    help="path to datasets folder",
+    help="path to datasets folder which contains da-tacos_benchmark_subset_single_files/ and da-tacos_coveranalysis_subset_single_files/ folders",
     default="datasets",
     show_default=True,
 )
@@ -55,56 +137,98 @@ def load(song, feature_type, spect_len):
     show_default=True,
 )
 @click.option(
-    "--spect-len", help="Resized Spectral Length", default=500, show_default=True,
+    "--spect-len",
+    "-s",
+    help="Resized output spectral length",
+    default=500,
+    show_default=True,
 )
-def preprocess(data_dir, pcp_features, spect_len):
+def da_tacos(data_dir, pcp_features, spect_len):
 
-    for dset_folder in ["benchmark", "coveranalysis"]:
+    for dset_folder in ("benchmark", "coveranalysis"):
         feature_dir = Path(data_dir) / f"da-tacos_{dset_folder}_subset_single_files"
-        store = pd.HDFStore(Path(data_dir) / f"{dset_folder}.h5", mode="w")
+        output_file = Path(data_dir) / f"{dset_folder}.h5"
+        dir_to_h5(feature_dir, output_file, pcp_features, spect_len)
 
-        data = list()
-        for track in feature_dir.glob("*/*"):
-            track_name = track.stem
-            work_name = track.parent.stem
-            data.append([work_name, track_name])
 
-        dataset_csv = pd.DataFrame(data, columns=["work_id", "track_id"])
-        work_id_max_len = dataset_csv.work_id.map(len).max()
-        track_id_max_len = dataset_csv.track_id.map(len).max()
-        rec = dataset_csv.to_records(index=False)
-        index = pd.MultiIndex.from_tuples(
-            [(*r, b) for r, b in itertools.product(rec, range(12))],
-            names=["work_id", "track_id", "chroma_bins"],
-        )
+@preprocess.command(
+    help="command to convert audio files directly to a single h5 file for testing and evaluation."
+)
+@click.option(
+    "--audio-dir",
+    "-a",
+    type=click.Path(exists=True),
+    help="path to audio folder",
+    required=True,
+)
+@click.option(
+    "--output-dir",
+    "-o",
+    type=click.Path(exists=True),
+    help="path to output folder, where the script will generate an intermediate features folder and single h5 file from that folder.",
+    default="datasets",
+    show_default=True,
+)
+@click.option(
+    "--pcp-features",
+    "-p",
+    type=click.Choice(["crema", "chroma_cens", "hpcp"], case_sensitive=False),
+    multiple=True,
+    help="select pcp feature (use this option multiple times for more than one selection)",
+    default=["crema"],
+    show_default=True,
+)
+@click.option(
+    "--run-mode",
+    "-r",
+    type=click.Choice(["parallel", "single"], case_sensitive=False),
+    help="Whether to run the extractor in single or parallel mode.",
+    default="parallel",
+    show_default=True,
+)
+@click.option(
+    "--workers",
+    "-n",
+    help="No of workers in parallel mode",
+    default=-1,
+    show_default=True,
+)
+@click.option(
+    "--spect-len",
+    "-s",
+    help="Resized output spectral length",
+    default=500,
+    show_default=True,
+)
+def from_audio_dir(audio_dir, output_dir, pcp_features, run_mode, workers, spect_len):
+    audio_dir = Path(audio_dir)
+    feature_dir = Path(output_dir) / f"{audio_dir.stem}_features"
+    output_file = Path(output_dir) / f"{audio_dir.stem}.h5"
 
-        for feature_type in pcp_features:
+    data_track_df = dir_to_df(audio_dir)
+    data_track_df.to_csv(
+        Path(output_dir) / f"{audio_dir.stem}_annotations.csv", index=False
+    )
 
-            feature_spec = np.empty(
-                (dataset_csv.shape[0], 12, spect_len), dtype=np.float32
-            )
+    extractor_profile = {
+        "sample_rate": 44100,
+        "input_audio_format": ".mp3",
+        "downsample_audio": False,
+        "downsample_factor": 2,
+        "endtime": 180,
+        "features": list(pcp_features),
+    }
 
-            for row in tqdm(
-                dataset_csv.itertuples(),
-                total=dataset_csv.shape[0],
-                desc=f"{dset_folder} - {feature_type}",
-            ):
-                track_file = feature_dir / row.work_id / row.track_id
-                feature_spec[row.Index, :, :] = load(
-                    track_file.with_suffix(".h5"), feature_type, spect_len
-                )
+    batch_feature_extractor(
+        dataset_csv=Path(output_dir) / f"{audio_dir.stem}_annotations.csv",
+        audio_dir=str(audio_dir) + "/",
+        feature_dir=str(feature_dir) + "/",
+        n_workers=workers,
+        mode=run_mode,
+        params=extractor_profile,
+    )
 
-            df = pd.DataFrame(
-                feature_spec.reshape(-1, feature_spec.shape[-1]), index=index
-            )
-
-            store.append(
-                feature_type,
-                df,
-                min_itemsize={"work_id": work_id_max_len, "track_id": track_id_max_len},
-            )
-
-        store.close()
+    dir_to_h5(feature_dir, output_file, pcp_features, spect_len)
 
 
 if __name__ == "__main__":
